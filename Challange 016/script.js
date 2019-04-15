4f1b785258f3a588cbbfc33c7197c1b7480fbe50
itsmarkodowd <godfather_mark@hotmail.com>
2019-04-15 19:12:33 +0100

    completed a few challanges

---
+++ Challange 016/script.js
@@ -0,0 +1,10 @@
+function digitize(n) {
+
+   var stringNum = n.toString();
+   var newArray = [];
+   
+   for (var i = 0; i < stringNum.length; i++){
+     newArray.push(parseInt(stringNum[i]));
+   }
+   return newArray.reverse();
+}
